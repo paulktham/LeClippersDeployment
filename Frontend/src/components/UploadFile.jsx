fee8485203160c3cabba4c990d4f430b521b4a3c
Paul <paultham7@gmail.com>
2024-05-29 20:28:57 +0800

    fixed the clear file bug

--- Frontend/src/components/UploadFile.jsx
+++ Frontend/src/components/UploadFile.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useRef } from "react";
 
 const UploadFile = ({ file, handleFileChange, handleClearFile }) => {
+  const fileInputRef = useRef(null);
+
+  const handleClear = () => {
+    handleClearFile();
+    fileInputRef.current.value = null; // Reset file input
+  };
+
   return (
     <div className="w-full min-h-screen bg-white items-center justify-center flex">
       <div className="w-[24%] h-auto rounded-md bg-white shadow-md border border-slate-400 p-5">
@@ -11,13 +18,19 @@ const UploadFile = ({ file, handleFileChange, handleClearFile }) => {
           Upload File
         </label>
         <div className="flex items-start w-full">
-          <input id="fileInput" type="file" onChange={handleFileChange} />
+          <input
+            id="fileInput"
+            type="file"
+            onChange={handleFileChange}
+            ref={fileInputRef}
+          />
           {file && (
-            <button onClick={handleClearFile} className="ml-2">
+            <button onClick={handleClear} className="ml-2">
               Clear
             </button>
           )}
         </div>
+        {file && <div className="mt-2">Selected File: {file}</div>}
       </div>
     </div>
   );
