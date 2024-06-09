80f080ec1ccbba41a39b27175f7d90b5feff725e
Paul <paultham7@gmail.com>
2024-06-09 15:41:21 +0800

    changed it such that if youre not logged in you will be directed to the login page and cant use the main page

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Header from "../components/Header";
 import UploadFile from "../components/UploadFile";
 import TimingList from "../components/TimingList";
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../context/authContext"; // Import the useAuth hook
 
 const Home = ({
   file,
@@ -11,6 +13,12 @@ const Home = ({
   handleSubmission,
   navigate,
 }) => {
+  const { userLoggedIn } = useAuth(); // Use the hook to get the userLoggedIn state
+
+  if (!userLoggedIn) {
+    return <Navigate to="/login" replace={true} />;
+  }
+
   return (
     <div>
       <Header />
