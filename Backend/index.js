656c1a2ca51110146918783386a8f474881f37a4
Paul <paultham7@gmail.com>
2024-06-25 20:59:50 +0800

    vercel server

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,7 @@ const port = 5000;
 
 app.use(
   cors({
-    origin: ["https://LeClippers.vercel.app"],
+    origin: ["https://LeClippersServer.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true,
   })
