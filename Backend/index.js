f515c0e4b09ee7fd6544da9c0662da5f4e9d27cc
Paul <paultham7@gmail.com>
2024-06-26 10:50:02 +0800

    private key fromating

--- Backend/index.js
+++ Backend/index.js
@@ -18,6 +18,7 @@ app.use(
 );
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
